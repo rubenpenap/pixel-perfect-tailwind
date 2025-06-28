7de09a6ff1aaf45315b7e597d11109a200cc90a5
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-06-27 22:02:51 -0400

    feat: wrap the word "Epic" in a span with highlight color in App component

--- exercises/01.project-setup/02.problem.tailwind-config/src/App.tsx
+++ exercises/01.project-setup/02.problem.tailwind-config/src/App.tsx
@@ -1,27 +1,25 @@
-import { EpicStackLogo, logos } from './logos/logos'
+import { EpicStackLogo, logos } from "./logos/logos";
 
 export default function App() {
-	return (
-		<>
-			<EpicStackLogo className="size-20" />
-			{/* 
-				🐨 Wrap the word "Epic" in a span, and set its text color
-				to "highlight", to verify our new color works.
-			*/}
-			<h1>The Epic Stack</h1>
-			<p>
-				Check the <a href="#">Getting Started</a> guide file for how to get your
-				project off the ground!
-			</p>
-			<ul>
-				{logos.map((logo) => (
-					<li key={logo.href}>
-						<a href={logo.href}>
-							<img src={logo.src} alt={logo.alt} className="w-16" />
-						</a>
-					</li>
-				))}
-			</ul>
-		</>
-	)
+  return (
+    <>
+      <EpicStackLogo className="size-20" />
+      <h1>
+        The <span className="text-highlight">Epic</span> Stack
+      </h1>
+      <p>
+        Check the <a href="#">Getting Started</a> guide file for how to get your
+        project off the ground!
+      </p>
+      <ul>
+        {logos.map((logo) => (
+          <li key={logo.href}>
+            <a href={logo.href}>
+              <img src={logo.src} alt={logo.alt} className="w-16" />
+            </a>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
