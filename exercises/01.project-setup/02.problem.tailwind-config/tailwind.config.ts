30b83ce1e2f8b38be9183b52682b2a56c5e1f788
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-06-27 22:02:46 -0400

    feat: update Tailwind CSS configuration with custom breakpoints, colors, font family, and font sizes

--- exercises/01.project-setup/02.problem.tailwind-config/tailwind.config.ts
+++ exercises/01.project-setup/02.problem.tailwind-config/tailwind.config.ts
@@ -1,46 +1,24 @@
-import { type Config } from 'tailwindcss'
+import { type Config } from "tailwindcss";
 
 export default {
-	content: [
-		/* 
-			🐨 Make sure that **everywhere** you use Tailwind classes
-			is included in this array.
-		*/
-		'./src/**/*.{ts,tsx,js,jsx}',
-	],
-	theme: {
-		extend: {
-			/* 
-				🐨 1. Extend the 'screens' object and replace 
-				the 'sm' and 'lg' breakpoints with the 
-				values from the Figma design.
-			*/
-			/* 
-				🐨 2. Extend the 'colors' object and add a 'highlight' color.
-				Get the HEX color value from Figma.
-			*/
-			/* 
-				🐨 3. Extend the 'fontFamily' object and add a 'poppins' family.
-				Set its value to `['Poppins', 'sans-serif']`.
-			*/
-			/*
-				🐨 4. Extend the 'fontSize' object to include the following:
-				
-				- a '4.5xl' key with: 
-					- a font size of '2.625rem' 
-					- a line height of '1.15'
-				
-				- a '5.5xl' key with:
-					- a font size of '3.375rem'
-					- a line height of '1'
-			
-				💰 You can use an array to pass both the 
-				font size and line hight like so:
-				fontSize: {
-					key: [ '2.625rem' // font size, '1.15' //line height ],
-				}
-			*/
-		},
-	},
-	plugins: [],
-} satisfies Config
+  content: ["./src/**/*.{ts,tsx,js,jsx}", "./index.html"],
+  theme: {
+    extend: {
+      screens: {
+        sm: "520px",
+        lg: "976px",
+      },
+      colors: {
+        highlight: "#6202FF",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      fontSize: {
+        "4.5xl": ["2.625rem", "1.25"],
+        "5.5xl": ["3.375rem", "1"],
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
