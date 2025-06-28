7608986584f29d9c2e61089446138248b8573f0c
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-06-28 12:36:33 -0400

    refactor: enhance App component layout with flexbox for better alignment and spacing

--- exercises/02.mobile-first/02.problem.text-styles/src/App.tsx
+++ exercises/02.mobile-first/02.problem.text-styles/src/App.tsx
@@ -1,48 +1,32 @@
-import { EpicStackLogo, logos } from './logos/logos'
+import { EpicStackLogo, logos } from "./logos/logos";
 
 export default function App() {
-	return (
-		<div className="grid min-h-screen place-items-center px-4 py-16">
-			<div className="grid place-items-center gap-12">
-				{/*
-					🐨
-						Set a max-width of 'md' on the wrapping div below.
-						Center the elements horizontally:
-					- Setup a veritcal flexbox container with 'flex flex-col'
-					- Align the items horizontally with 'items-center'
-					- Center the children text elements with 'text-center'
-				*/}
-				<div>
-					<EpicStackLogo className="size-20" />
-					{/*
-						🐨 Apply the correct font size, weight and spacing to the h1.
-					*/}
-					<h1>
-						The <span className="text-highlight">Epic</span> Stack
-					</h1>
-
-					{/*
-						🐨 Apply the correct spacing and color to the paragraph.
-					*/}
-					<p>
-						{/*
-							🐨 Apply the correct text color and decoration to the link.
-							Don't worry about the hover state for now.
-						*/}
-						Check the <a href="#">Getting Started</a> guide file for how to get
-						your project off the ground!
-					</p>
-				</div>
-				<ul>
-					{logos.map((logo) => (
-						<li key={logo.href}>
-							<a href={logo.href}>
-								<img src={logo.src} alt={logo.alt} className="w-16" />
-							</a>
-						</li>
-					))}
-				</ul>
-			</div>
-		</div>
-	)
+  return (
+    <div className="grid min-h-screen place-items-center px-4 py-16">
+      <div className="grid place-items-center gap-12">
+        <div className="flex flex-col items-center text-center max-w-md">
+          <EpicStackLogo className="size-20" />
+          <h1 className="text-4xl font-medium mt-6">
+            The <span className="text-highlight">Epic</span> Stack
+          </h1>
+          <p className="text-slate-600 mt-4">
+            Check the{" "}
+            <a className="text-black underline" href="#">
+              Getting Started
+            </a>{" "}
+            guide file for how to get your project off the ground!
+          </p>
+        </div>
+        <ul>
+          {logos.map((logo) => (
+            <li key={logo.href}>
+              <a href={logo.href}>
+                <img src={logo.src} alt={logo.alt} className="w-16" />
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
