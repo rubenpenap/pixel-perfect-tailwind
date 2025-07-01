072504bb60c1afb1437c4eda3188c6ca500da349
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-07-01 13:05:16 -0400

    refactor: integrate Tailwind CSS plugin into Vite configuration for improved styling capabilities

--- exercises/08.upgrade-to-tailwind-v4/01.problem.vite-plugin/vite.config.ts
+++ exercises/08.upgrade-to-tailwind-v4/01.problem.vite-plugin/vite.config.ts
@@ -1,13 +1,11 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-
-// 🐨 Import the `@tailwindcss/vite` plugin
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	// 🐨 Register the Taliwind plugin in the `plugins` array
-	plugins: [react()],
-	server: {
-		port: Number(process.env.PORT) || 3000,
-	},
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: Number(process.env.PORT) || 3000,
+  },
+});
