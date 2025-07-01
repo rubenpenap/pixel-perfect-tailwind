cbf46f2e4d8d3bd51ae6c9fd9da29a15fa7075f4
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-06-30 20:30:39 -0400

    refactor: add 'slide-left' keyframe and animation to tailwind configuration for enhanced entrance animations

--- exercises/06.animation/02.problem.responsive-animations/tailwind.config.ts
+++ exercises/06.animation/02.problem.responsive-animations/tailwind.config.ts
@@ -1,41 +1,38 @@
-import { type Config } from 'tailwindcss'
+import { type Config } from "tailwindcss";
 
 export default {
-	content: ['./src/**/*.{ts,tsx,js,jsx}', './index.html'],
-	theme: {
-		extend: {
-			screens: {
-				sm: '520px',
-				lg: '976px',
-			},
-			colors: {
-				highlight: '#6202FF',
-			},
-			fontFamily: {
-				poppins: ['Poppins', 'sans-serif'],
-			},
-			fontSize: {
-				'4.5xl': ['2.625rem', '1.15'],
-				'5.5xl': ['3.375rem', '1'],
-			},
-			keyframes: {
-				/* 
-					🐨 Add a 'slide-left' keyframe. It should be just like the 'slide-top' 
-					keyframe, but with the translate happening on the X axis.
-				*/
-				'slide-top': {
-					from: { transform: 'translateY(20px)', opacity: '0' },
-					to: { transform: 'translateY(0px)', opacity: '1' },
-				},
-			},
-			animation: {
-				/* 
-					🐨 Add a 'slide-left' animation, which replicates the 'slide-top' 
-					animation, but with the 'slide-left' keyframe.
-				*/
-				'slide-top': '0.3s slide-top ease-out backwards',
-			},
-		},
-	},
-	plugins: [],
-} satisfies Config
+  content: ["./src/**/*.{ts,tsx,js,jsx}", "./index.html"],
+  theme: {
+    extend: {
+      screens: {
+        sm: "520px",
+        lg: "976px",
+      },
+      colors: {
+        highlight: "#6202FF",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      fontSize: {
+        "4.5xl": ["2.625rem", "1.15"],
+        "5.5xl": ["3.375rem", "1"],
+      },
+      keyframes: {
+        "slide-left": {
+          from: { transform: "translateX(20px)", opacity: "0" },
+          to: { transform: "translateX(0px)", opacity: "1" },
+        },
+        "slide-top": {
+          from: { transform: "translateY(20px)", opacity: "0" },
+          to: { transform: "translateY(0px)", opacity: "1" },
+        },
+      },
+      animation: {
+        "slide-left": "0.3s slide-left ease-out backwards",
+        "slide-top": "0.3s slide-top ease-out backwards",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
